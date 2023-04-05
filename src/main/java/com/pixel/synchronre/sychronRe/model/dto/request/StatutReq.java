c4b93158272ab6c0ca0f413bdd9ec6cf478182f8
Youin Salif <salifyouin@gmail.com>
2023-04-05 16:33:31 +0000

    module statut:creation des dto et mapper

---
+++ src/main/java/com/pixel/synchronre/sychronRe/model/dto/request/StatutReq.java
@@ -0,0 +1,27 @@
+package com.pixel.synchronre.sychronRe.model.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class StatutReq {
+    @NotBlank(message = "Veuillez saisir le code")
+    @NotNull(message = "Veuillez saisir le code")
+    @Length(message = "Le code doit contenir au moins 3 caractères", min = 3)
+    private String staCode;
+
+    @NotBlank(message = "Veuillez saisir le libelle")
+    @NotNull(message = "Veuillez saisir le libelle")
+    @Length(message = "Le libelle doit contenir au moins 3 caractères", min = 3)
+    private String staLibelle;
+
+    private String staLibelleAbrege;
+}
