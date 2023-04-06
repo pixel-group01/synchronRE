31de09978aae101eda4c9846e1357725ef565033
ghislainatsin01 <ghislainatsin01@gmail.com>
2023-04-06 23:52:12 +0100

    MAJ : Parametrage des logs pour Banque et Couverture

--- src/main/java/com/pixel/synchronre/sychronremodule/model/constants/SynchronReActions.java
+++ src/main/java/com/pixel/synchronre/sychronremodule/model/constants/SynchronReActions.java
@@ -4,4 +4,10 @@ public class SynchronReActions
 {
     public static final String CREATE_CESSIONNAIRE = "Création d'un cessionnaire";
     public static final String UPDATE_CESSIONNAIRE = "Modification d'un cessionnaire";
+    public static final String CREATE_BANQUE = "Création d'une banque";
+    public static final String UPDATE_BANQUE = "Modification d'une banque";
+    public static final String CREATE_BRANCHE = "Création d'une branche";
+    public static final String UPDATE_BRANCHE = "Modification d'une branche";
+    public static final String CREATE_COUVERTURE = "Création d'une couverture";
+    public static final String UPDATE_COUVERTURE = "Modification d'une couverture";
 }
--- src/main/java/com/pixel/synchronre/sychronremodule/model/constants/SynchronReTables.java
+++ src/main/java/com/pixel/synchronre/sychronremodule/model/constants/SynchronReTables.java
@@ -2,4 +2,6 @@ package com.pixel.synchronre.sychronremodule.model.constants;
 
 public class SynchronReTables {
     public static final String CESSIONNAIRE = "Cessionnaire";
+    public static final String BANQUE = "Banque";
+    public static final String COUVERTURE = "Couverture";
 }
