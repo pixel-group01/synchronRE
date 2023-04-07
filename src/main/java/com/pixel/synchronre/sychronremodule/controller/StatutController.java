a3809d7e5411bde671c411eff7979cb51040d6c1
Youin Salif <salifyouin@gmail.com>
2023-04-07 00:11:15 +0000

    Sous module Statut : Ajout de StatutController

---
+++ src/main/java/com/pixel/synchronre/sychronremodule/controller/StatutController.java
@@ -0,0 +1,38 @@
+package com.pixel.synchronre.sychronremodule.controller;
+import com.pixel.synchronre.sychronremodule.model.dto.statut.request.CreateStatutReq;
+import com.pixel.synchronre.sychronremodule.model.dto.statut.request.UpdateStatutReq;
+import com.pixel.synchronre.sychronremodule.model.dto.statut.response.StatutDetailsResp;
+import com.pixel.synchronre.sychronremodule.model.dto.statut.response.StatutListResp;
+import com.pixel.synchronre.sychronremodule.service.interfac.StatutIservice;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.UnknownHostException;
+
+
+@RestController
+@RequestMapping("/statuts")
+@RequiredArgsConstructor
+public class StatutController {
+    private final StatutIservice statutService;
+    
+    @PostMapping(path = "/create")
+    public StatutDetailsResp createStatut(@RequestBody @Valid CreateStatutReq dto) throws UnknownHostException {
+        return statutService.createStatut(dto);
+    }
+
+    @PutMapping(path = "/update")
+    public StatutDetailsResp updateStatut(@RequestBody @Valid UpdateStatutReq dto) throws UnknownHostException {
+        return statutService.updateStatut(dto);
+    }
+
+    @GetMapping(path = "/list")
+    public Page<StatutListResp> searchStatuts(@RequestParam(defaultValue = "") String key, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) throws UnknownHostException {
+        return statutService.searchStatut(key, PageRequest.of(page, size));
+    }
+
+}
