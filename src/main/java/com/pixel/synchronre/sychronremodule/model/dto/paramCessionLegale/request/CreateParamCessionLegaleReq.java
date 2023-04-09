9e32d5d581d3893fc477aede6b9a244f61630845
ghislainatsin01 <ghislainatsin01@gmail.com>
2023-04-09 02:56:29 +0100

    ADD : MODULE PARAM CESSION LEGALE ET CORRECTION CEDANTE-Suite

--- src/main/java/com/pixel/synchronre/sychronremodule/model/dto/paramCessionLegale/request/CreateParamCessionLegaleReq.java
+++ src/main/java/com/pixel/synchronre/sychronremodule/model/dto/paramCessionLegale/request/CreateParamCessionLegaleReq.java
@@ -13,16 +13,13 @@ public class CreateParamCessionLegaleReq
     @NotBlank(message = "Veuillez saisir le libéllé du paramètre de la cession légale")
     @NotNull(message = "Veuillez saisir le libéllé du paramètre de la cession légale")
     private String paramCesLegLibelle;
-    @NotBlank(message = "Veuillez saisir le capital de la cession légale")
     @NotNull(message = "Veuillez saisir le capital de la cession légale")
     private Long paramCesLegCapital;
-    @NotBlank(message = "Veuillez saisir le taux de la cession légale")
     @NotNull(message = "Veuillez saisir le taux de la cession légale")
     private float paramCesLegTaux;
     @NotBlank(message = "Veuillez saisir le pays")
     @NotNull(message = "Veuillez saisir le pays")
     private String paysCode;
-    @NotBlank(message = "Veuillez choisir la cédante")
     @NotNull(message = "Veuillez  choisir la cédante")
     private Long cedId;
 
