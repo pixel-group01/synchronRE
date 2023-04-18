9e13860d2daddf168ac4557a8ad4057141786ee6
VENANCE ADOU <venance.adou@smile.ci>
2023-04-18 11:52:39 +0000

    bug fix:cors origin

--- src/main/java/com/pixel/synchronre/authmodule/config/SecurityConfig.java
+++ src/main/java/com/pixel/synchronre/authmodule/config/SecurityConfig.java
@@ -22,8 +22,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Base64;
+import java.util.Collections;
 
 @Configuration @EnableWebSecurity
 @RequiredArgsConstructor
@@ -37,7 +40,16 @@ public class SecurityConfig
         return httpSecurity
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+                .and().cors(cors -> {
+                    CorsConfigurationSource source = request -> {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("*"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        return config;
+                    };
+                    cors.configurationSource(source);
+                })
                 .authorizeHttpRequests()
                 .requestMatchers("/*/open/**","/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated()
