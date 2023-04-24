fa3b9debdad2727cf240374395713128303e19d5
lenicoulibaly <lenicode777@gmail.com>
2023-04-24 01:51:07 +0000

    Integration des états avec jasper : test2

---
+++ src/main/java/com/pixel/synchronre/reportmodule/service/interfac/IReportExporter.java
@@ -0,0 +1,17 @@
+package com.pixel.synchronre.reportmodule.service.interfac;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperPrint;
+
+import java.io.File;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+public interface IReportExporter
+{
+    File exportReport(JasperPrint jasperPrint, String destinationFilePath) throws JRException;
+    File exportReport(String reportSourcePath, List<Object> datas, Map<String, Object> parameters, String destinationFilePath) throws JRException;
+    File exportReport(String reportSourcePath, Map<String, Object> parameters, String destinationFilePath) throws JRException, SQLException;
+    File exportReport(String reportSourcePath, Map<String, Object> parameters) throws JRException, SQLException;
+}
