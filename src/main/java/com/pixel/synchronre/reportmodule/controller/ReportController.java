fc3dea2b7e1ac1aed681e8c2a04391bd879dc82b
lenicoulibaly <lenicode777@gmail.com>
2023-04-25 02:28:18 +0000

    Correction de la requete de searchAffaire

--- src/main/java/com/pixel/synchronre/reportmodule/controller/ReportController.java
+++ src/main/java/com/pixel/synchronre/reportmodule/controller/ReportController.java
@@ -1,10 +1,25 @@
 package com.pixel.synchronre.reportmodule.controller;
 
+import com.pixel.synchronre.reportmodule.service.interfac.IReportExporter;
+import lombok.RequiredArgsConstructor;
+import net.sf.jasperreports.engine.JRException;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.io.File;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+@RestController @RequestMapping("/reports")
+@RequiredArgsConstructor @PreAuthorize("permitAll()")
 public class ReportController
 {
-
+    private final IReportExporter reportExporter;
+    @GetMapping(path = "/test")
+    File test() throws JRException, SQLException {
+        return reportExporter.exportReport("src/main/resources/report2/test.jrxml", new HashMap<>(), "C:\\Users\\DMP_Portable\\Desktop\\report\\test2.pdf");
+    }
 }
