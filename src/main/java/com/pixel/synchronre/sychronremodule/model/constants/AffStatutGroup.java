1e70a393421dbc97e476dd0f835fde6f29641118
ghislainatsin01 <ghislainatsin01@gmail.com>
2023-05-05 12:52:22 +0100

    Test1

--- src/main/java/com/pixel/synchronre/sychronremodule/model/constants/AffStatutGroup.java
+++ src/main/java/com/pixel/synchronre/sychronremodule/model/constants/AffStatutGroup.java
@@ -9,12 +9,12 @@ public class AffStatutGroup
 {
     public static final List<String> tabSaisieCed = Arrays.asList(SAISIE.staCode, SAISIE_CRT.staCode, RETOURNE.staCode, EN_COURS_DE_REPARTITION.staCode);
     public static final List<String> tabEnCoursPla = Arrays.asList(EN_ATTENTE_DE_PLACEMENT.staCode, EN_COURS_DE_PLACEMENT.staCode);
-    public static final List<String> tabEnCoursReg = Arrays.asList(EN_ATTENTE_DE_REGLEMENT.staCode, EN_COURS_DE_REGLEMENT.staCode);
+    public static final List<String> tabEnCoursReg = Arrays.asList(EN_ATTENTE_DE_REGLEMENT.staCode, EN_COURS_DE_REGLEMENT.staCode,SOLDE.staCode);
     public List<String> tabEnCoursRegSous = Arrays.asList(EN_ATTENTE_DE_REGLEMENT.staCode, EN_COURS_DE_REGLEMENT.staCode);
     public static final List<String> tabSaisieSous = Arrays.asList(SAISIE_CRT.staCode, TRANSMIS.staCode);
     public static final List<String> tabArchives = Arrays.asList(EN_ATTENTE_DE_VALIDATION.staCode, VALIDE.staCode, MAIL.staCode);
     public static final List<String> tabAllAffaires = Arrays.asList(SAISIE.staCode,RETOURNE.staCode,EN_ATTENTE_DE_VALIDATION.staCode,EN_ATTENTE_DE_PLACEMENT.staCode, EN_COURS_DE_PLACEMENT.staCode,
                                                                     EN_COURS_DE_REPARTITION.staCode,VALIDE.staCode, MAIL.staCode,SAISIE_CRT.staCode, TRANSMIS.staCode,
-                                                                    EN_ATTENTE_DE_REGLEMENT.staCode, EN_COURS_DE_REGLEMENT.staCode
+                                                                    EN_ATTENTE_DE_REGLEMENT.staCode, EN_COURS_DE_REGLEMENT.staCode, SOLDE.staCode
                                                                     );
 }
