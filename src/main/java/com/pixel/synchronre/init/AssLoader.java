e9d26ccd5402fef187b13418ac301b41ad66366a
salif <salifyouin@gmail.com>
2023-07-04 23:07:01 +0000

    Assignation opeFac,opeSin et opeTrai

--- src/main/java/com/pixel/synchronre/init/AssLoader.java
+++ src/main/java/com/pixel/synchronre/init/AssLoader.java
@@ -39,6 +39,19 @@ public class AssLoader implements Loader
         List<AppPrivilege> prvAdminFonc = prvRepo.findByPrvCodes(Arrays.asList("GET-STAT-TRAI", "GET-STAT-FAC", "GET-STAT-SIN", "GET-STAT-ADM", "GET-STAT-PARAM", "CRT-BANK", "UPD-BANK", "GET-BANK-LST", "CRT-BRAN", "UPD-BRAN", "GET-BRAN-LST", "CRT-CED", "UPD-CED", "GET-CED-LST", "CRT-CES", "UPD-CES", "GET-CES-LEG-PARAM-LST", "CRT-CES-LEG-PARAM", "UPD-CES-LEG-PARAM", "GET-CES-LST", "CRT-COUV", "UPD-COUV", "GET-COUV-LST", "CRT-DEV", "UPD-DEV", "GET-DEV-LST", "CRT-EXE", "UPD-EXE", "GET-EXE-LST", "CRT-PAY", "UPD-PAY", "GET-PAY-LST"));
         prvAdminFonc.forEach(prv->this.addPrvToRole(prv, roleAdminFonc));
 
+        AppRole roleOpeSaisieFac = roleRepo.findByRoleCode("ROL-OPE-SAI-FAC");
+        List<AppPrivilege> prvOpeSaisieFac = prvRepo.findByPrvCodes(Arrays.asList("GET-STAT-FAC","GET-FAC-LST","GET-FAC-C-SAI","GET-FAC-C-PLA","GET-FAC-C-REG","GET-FAC-ARCH","GET-FAC-HIST","CRT-FAC","UPD-FAC","GET-FAC-DET","SEND-FAC","CRT-PLA-FAC","VAL-FAC","RET-FAC","ARCH-FAC","MARK-FAC-REA","MARK-FAC-NON-REA","ADD-DOC-FAC","GET-FAC-MOUV","GET-FAC-REFU-MSG","GET-FAC-RET-MSG","GET-PLA-RET-MSG"));
+        prvOpeSaisieFac.forEach(prv->this.addPrvToRole(prv, roleOpeSaisieFac));
+
+        AppRole roleOpeSaisieSin = roleRepo.findByRoleCode("ROL-OPE-SAI-SIN");
+        List<AppPrivilege> prvOpeSaisieSin = prvRepo.findByPrvCodes(Arrays.asList("GET-STAT-SIN","CRT-SIN","UPD-SIN","DEL-SIN","TRANS-SIN","GET-SIN-LST","GET-SIN-FAC-LST","GET-SIN-TRAI-LST"));
+        prvOpeSaisieSin.forEach(prv->this.addPrvToRole(prv, roleOpeSaisieSin));
+
+        AppRole roleOpeSaisieTrai = roleRepo.findByRoleCode("ROL-OPE-SAI-TRAI");
+        List<AppPrivilege> prvOpeSaisieTrai = prvRepo.findByPrvCodes(Arrays.asList("GET-STAT-TRAI"));
+        prvOpeSaisieTrai.forEach(prv->this.addPrvToRole(prv, roleOpeSaisieTrai));
+
+
 
     }
 }
