9dbab2d6765b3e2b3985c766ff8ebfb34f4330a8
salif <salifyouin@gmail.com>
2023-07-12 18:28:21 +0000

    Affichage des messages de retour des sinistres

--- src/main/java/com/pixel/synchronre/sychronremodule/controller/MvtController.java
+++ src/main/java/com/pixel/synchronre/sychronremodule/controller/MvtController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController @RequiredArgsConstructor @RequestMapping(path = "/mouvements") @ResponseStatus(HttpStatus.OK)
 public class MvtController
 {
-    private MouvementRepository mvtRepo;
+    private final MouvementRepository mvtRepo;
 
     @GetMapping(path = "/affaire/get-message-retour/{affId}")
     String getMessageRetourForAffaire(@PathVariable Long affId)
@@ -20,5 +20,23 @@ public class MvtController
     String getMessageRetourForPlacement(@PathVariable Long plaId)
     {
         return mvtRepo.getMessageRetourForPlacement(plaId);
+    }
+
+    @GetMapping(path = "/sinistre/get-message-retour-comptable/{sinId}")
+    String getMessageRetourComptaForSinistre(@PathVariable Long sinId)
+    {
+        return mvtRepo.getMvtMessage(null, null, sinId, "RET-COMPTA");
+    }
+
+    @GetMapping(path = "/sinistre/get-message-retour-validateur/{sinId}")
+    String getMessageRetourValidateurForSinistre(@PathVariable Long sinId)
+    {
+        return mvtRepo.getMvtMessage(null, null, sinId, "RET-VAL");
+    }
+
+    @GetMapping(path = "/sinistre/get-message-retour-souscripteur/{sinId}")
+    String getMessageRetourSouscripteurForSinistre(@PathVariable Long sinId)
+    {
+        return mvtRepo.getMvtMessage(null, null, sinId, "RET");
     }
 }
