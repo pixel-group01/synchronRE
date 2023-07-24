839524ca4ed4f5d85ee5cffc265d93edd52bffdd
lenicoulibaly <lenicode777@gmail.com>
2023-07-24 23:23:30 +0000

    Retour de base64Url pour la génération des états

--- src/main/java/com/pixel/synchronre/reportmodule/controller/ReportRestController.java
+++ src/main/java/com/pixel/synchronre/reportmodule/controller/ReportRestController.java
@@ -1,9 +1,11 @@
 package com.pixel.synchronre.reportmodule.controller;
 
 import com.pixel.synchronre.archivemodule.controller.service.AbstractDocumentService;
+import com.pixel.synchronre.archivemodule.model.dtos.response.Base64FileDto;
 import com.pixel.synchronre.reportmodule.config.JasperReportConfig;
 import com.pixel.synchronre.reportmodule.service.IServiceReport;
 import com.pixel.synchronre.sharedmodule.exceptions.AppException;
+import com.pixel.synchronre.sharedmodule.utilities.Base64ToFileConverter;
 import com.pixel.synchronre.sychronremodule.model.dao.AffaireRepository;
 import com.pixel.synchronre.sychronremodule.model.dao.ReglementRepository;
 import com.pixel.synchronre.sychronremodule.model.dao.RepartitionRepository;
@@ -14,18 +16,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-//@Controller @RequestMapping(path = "/reports")
-@RequiredArgsConstructor @ResponseStatus(HttpStatus.OK)
-public class ReportController
+@RestController @RequestMapping(path = "/reports") @RequiredArgsConstructor @ResponseStatus(HttpStatus.OK)
+public class ReportRestController
 {
     private final IServiceReport jrService;
     private final JasperReportConfig jrConfig;
@@ -35,7 +33,7 @@ public class ReportController
     private final AbstractDocumentService docService;
 
     @GetMapping("/note-cession/{plaId}")
-    public void generateNoteCession(HttpServletResponse response, @PathVariable Long plaId) throws Exception
+    public Base64FileDto generateNoteCession(HttpServletResponse response, @PathVariable Long plaId) throws Exception
     {
         Repartition placement = repRepo.findById(plaId).orElseThrow(()-> new AppException("Placement introuvable"));
         if(!placement.getType().getUniqueCode().equals("REP_PLA")) throw new AppException("Cette repartition n'est pas un placement");
@@ -45,21 +43,19 @@ public class ReportController
         params.put("fac_numero_police", placement.getAffaire().getFacNumeroPolice());
         params.put("ces_id", placement.getCessionnaire().getCesId());
         params.put("param_image", jrConfig.imagesLocation);
-            //@Charly
-            //Affichage du cachet en fonction d'une expression dans l'etat
         if (placement.getRepStaCode().getStaCode().equals("VAL") || placement.getRepStaCode().getStaCode().equals("MAIL")){
             params.put("param_visible", "true");
         }
         else{
             params.put("param_visible", "false");
         }
-            //fin de la manip
         byte[] reportBytes = jrService.generateReport(jrConfig.noteCession, params, new ArrayList<>(), null);
-        docService.displayPdf(response, reportBytes, "Note-de-cession");
+        String base64Url = Base64ToFileConverter.convertBytesToBase64UrlString(reportBytes);
+        return new Base64FileDto(base64Url);
     }
 
     @GetMapping("/note-de-debit/{affId}")
-    public void generateNoteDebit(HttpServletResponse response, @PathVariable Long affId) throws Exception
+    public Base64FileDto generateNoteDebit(HttpServletResponse response, @PathVariable Long affId) throws Exception
     {
         Repartition repart = repRepo.repartFindByAffaire(affId).orElseThrow(()-> new AppException("Affaire introuvable"));
 
@@ -77,11 +73,12 @@ public class ReportController
         }
 
         byte[] reportBytes = jrService.generateReport(jrConfig.noteDebit, params, new ArrayList<>(), null);
-        docService.displayPdf(response, reportBytes, "Note-de-debit");
+        String base64Url = Base64ToFileConverter.convertBytesToBase64UrlString(reportBytes);
+        return new Base64FileDto(base64Url);
     }
 
     @GetMapping("/note-de-credit/{affId}/{cesId}")
-    public void generateNoteCredit(HttpServletResponse response, @PathVariable Long affId, @PathVariable Long cesId) throws Exception
+    public Base64FileDto generateNoteCredit(HttpServletResponse response, @PathVariable Long affId, @PathVariable Long cesId) throws Exception
     {
         Repartition placement = repRepo.getPlacementByAffIdAndCesId(affId,cesId).orElseThrow(()-> new AppException("Placement introuvable"));
         if(!placement.getType().getUniqueCode().equals("REP_PLA")) throw new AppException("Cette repartition n'est pas un placement");
@@ -92,11 +89,12 @@ public class ReportController
         params.put("ces_id", placement.getCessionnaire().getCesId());
         params.put("param_image", jrConfig.imagesLocation);
         byte[] reportBytes = jrService.generateReport(jrConfig.noteCredit, params, new ArrayList<>(), null);
-        docService.displayPdf(response, reportBytes, "Note-de-credit");
+        String base64Url = Base64ToFileConverter.convertBytesToBase64UrlString(reportBytes);
+        return new Base64FileDto(base64Url);
     }
 
     @GetMapping("/note-cession-sinistre/{plaId}")
-    public void generateNoteCessionSinistre(HttpServletResponse response, @PathVariable Long plaId) throws Exception
+    public Base64FileDto generateNoteCessionSinistre(HttpServletResponse response, @PathVariable Long plaId) throws Exception
     {
         Repartition placement = repRepo.findById(plaId).orElseThrow(()-> new AppException("Placement introuvable"));
         if(!placement.getType().getUniqueCode().equals("REP_PLA")) throw new AppException("Cette repartition n'est pas un placement");
@@ -114,11 +112,12 @@ public class ReportController
             params.put("param_visible", "false");
         }
         byte[] reportBytes = jrService.generateReport(jrConfig.noteCessionSinistre, params, new ArrayList<>(), null);
-        docService.displayPdf(response, reportBytes, "Note-cession-sinistre");
+        String base64Url = Base64ToFileConverter.convertBytesToBase64UrlString(reportBytes);
+        return new Base64FileDto(base64Url);
     }
 
     @GetMapping("/note-debit-sinistre/{affId}")
-    public void generateNoteDebitSinistre(HttpServletResponse response, @PathVariable Long affId) throws Exception
+    public Base64FileDto generateNoteDebitSinistre(HttpServletResponse response, @PathVariable Long affId) throws Exception
     {
         Affaire affaire = affRepo.findById(affId).orElseThrow(()-> new AppException("Affaire introuvable"));
         Map<String, Object> params = new HashMap<>();
@@ -127,16 +126,18 @@ public class ReportController
         params.put("fac_numero_police", affaire.getFacNumeroPolice());
         params.put("param_image", jrConfig.imagesLocation);
         byte[] reportBytes = jrService.generateReport(jrConfig.noteDebitSinistre, params, new ArrayList<>(), null);
-        docService.displayPdf(response, reportBytes, "Note-Debit-Sinistre");
+        String base64Url = Base64ToFileConverter.convertBytesToBase64UrlString(reportBytes);
+        return new Base64FileDto(base64Url);
     }
 
     @GetMapping("/cheque/{regId}")
-    public void generateCheque(HttpServletResponse response, @PathVariable Long regId) throws Exception
+    public Base64FileDto generateCheque(HttpServletResponse response, @PathVariable Long regId) throws Exception
     {
         Reglement reglement =  regRepo.findById(regId).orElseThrow(()-> new AppException(("Règlement introuvable")));
         Map<String, Object> params = new HashMap<>();
         params.put("reg_id", reglement.getRegId());
         byte[] reportBytes = jrService.generateReport(jrConfig.cheque, params, new ArrayList<>(), null);
-        docService.displayPdf(response, reportBytes, "Cheque");
+        String base64Url = Base64ToFileConverter.convertBytesToBase64UrlString(reportBytes);
+        return new Base64FileDto(base64Url);
     }
 }
