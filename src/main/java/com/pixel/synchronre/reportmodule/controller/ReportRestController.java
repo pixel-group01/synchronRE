5ddabfc271d223052bd1addd5d3030a72736ec13
lenicoulibaly <lenicode777@gmail.com>
2023-07-31 21:22:28 +0000

    Impression de la note de cession et note de debit fac

--- src/main/java/com/pixel/synchronre/reportmodule/controller/ReportRestController.java
+++ src/main/java/com/pixel/synchronre/reportmodule/controller/ReportRestController.java
@@ -1,16 +1,32 @@
 package com.pixel.synchronre.reportmodule.controller;
 
+import com.pixel.synchronre.archivemodule.controller.service.AbstractDocumentService;
 import com.pixel.synchronre.archivemodule.model.dtos.response.Base64FileDto;
+import com.pixel.synchronre.reportmodule.config.JasperReportConfig;
 import com.pixel.synchronre.reportmodule.service.IServiceReport;
+import com.pixel.synchronre.sharedmodule.exceptions.AppException;
 import com.pixel.synchronre.sharedmodule.utilities.Base64ToFileConverter;
+import com.pixel.synchronre.sychronremodule.model.dao.AffaireRepository;
+import com.pixel.synchronre.sychronremodule.model.dao.ReglementRepository;
+import com.pixel.synchronre.sychronremodule.model.dao.RepartitionRepository;
+import com.pixel.synchronre.sychronremodule.model.entities.Repartition;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController @RequestMapping(path = "/reports") @RequiredArgsConstructor @ResponseStatus(HttpStatus.OK)
 public class ReportRestController
 {
     private final IServiceReport jrService;
+
+    private final JasperReportConfig jrConfig;
+    private final RepartitionRepository repRepo;
+    private final AbstractDocumentService docService;
 
     @GetMapping("/note-cession-fac/{plaId}")
     public Base64FileDto generateNoteCession(@PathVariable Long plaId) throws Exception
@@ -59,5 +75,19 @@ public class ReportRestController
         byte[] reportBytes = jrService.generateCheque(regId);
         String base64Url = Base64ToFileConverter.convertBytesToBase64UrlString(reportBytes).replace("_", "/").replace("-", "+");
         return new Base64FileDto(base64Url, reportBytes);
+    }
+
+    @GetMapping("/display-note-de-debit-fac/{affId}")
+    public void generateNoteDebit(HttpServletResponse response, @PathVariable Long affId) throws Exception
+    {
+        byte[] reportBytes = jrService.generateNoteDebitFac(affId);
+        docService.displayPdf(response, reportBytes, "Note-de-debit");
+    }
+
+    @GetMapping("/display-note-de-cession-fac/{plaId}")
+    public void generateNoteCession(HttpServletResponse response, @PathVariable Long plaId) throws Exception
+    {
+        byte[] reportBytes = jrService.generateNoteCessionFac(plaId);
+        docService.displayPdf(response, reportBytes, "Note-de-cession");
     }
 }
--- src/main/java/com/pixel/synchronre/reportmodule/service/IServiceReport.java
+++ src/main/java/com/pixel/synchronre/reportmodule/service/IServiceReport.java
@@ -1,12 +1,7 @@
 package com.pixel.synchronre.reportmodule.service;
 
-import jakarta.servlet.http.HttpServletResponse;
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperPrint;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.io.File;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
