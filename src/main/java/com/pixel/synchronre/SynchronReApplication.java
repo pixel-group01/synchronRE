d1a047e619daa8794b68d02a5ca1303963a7231c
salifyouin <salifyouin@gmail.com>
2024-05-06 17:52:22 +0000

    Modelisation du sous module traite non proportionnel

--- src/main/java/com/pixel/synchronre/SynchronReApplication.java
+++ src/main/java/com/pixel/synchronre/SynchronReApplication.java
@@ -2,6 +2,7 @@ package com.pixel.synchronre;
 
 import com.pixel.synchronre.init.*;
 import com.pixel.synchronre.sychronremodule.model.dao.CessionnaireRepository;
+import com.pixel.synchronre.sychronremodule.model.entities.Organisation;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,12 +28,13 @@ public class SynchronReApplication {
     public static void main(String[] args) {
         SpringApplication.run(SynchronReApplication.class, args);
     }
-    @Bean(name = "commandLineRunner") @Profile("dev")
+    @Bean(name = "commandLineRunner") //@Profile("dev")
     public CommandLineRunner start(TypeLoader typeLoader, FoldersIniter foldersIniter, AdminLoader adminLoader, StatutLoader statutLoader,
                                    DeviseLoader deviseLoader, PaysLoader paysLoader, PclLoader pclLoader,
                                    BrancheLoader brancheLoader, CouvertureLoader couvertureLoader,
-                                   CedCesAffLoader cedCesAffLoader, ExerciceLoader exerciceLoader, CessionnaireRepository cesRepo
-                                  ,PrvLoader prvLoader, RoleLoader roleLoader,MenuLoader menuLoader, AssLoader assLoader, NatureLoader natureLoader)
+                                   CedCesAffLoader cedCesAffLoader, ExerciceLoader exerciceLoader
+                                  ,NatureLoader natureLoader, PrvLoader prvLoader, RoleLoader roleLoader, MenuLoader menuLoader, AssLoader assLoader,OrganisationLoader organisationLoader,
+                                  OrganisationPaysLoader organisationPaysLoader)
     {
         return args->{
             typeLoader.load();
@@ -46,7 +48,9 @@ public class SynchronReApplication {
             deviseLoader.load();
             brancheLoader.load();
             couvertureLoader.load();
+            organisationLoader.load();
             paysLoader.load();
+            organisationPaysLoader.load();
             pclLoader.load();
             exerciceLoader.load();
             cedCesAffLoader.load();
