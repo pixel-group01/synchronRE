9fafb29ebf175092d8700762304fb9af9596e324
lenicoulibaly <lenicoulibaly@gmail.com>
2024-05-06 17:43:23 +0000

    Chargement des natures au démarrage

--- src/main/java/com/pixel/synchronre/SynchronReApplication.java
+++ src/main/java/com/pixel/synchronre/SynchronReApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
@@ -26,12 +27,12 @@ public class SynchronReApplication {
     public static void main(String[] args) {
         SpringApplication.run(SynchronReApplication.class, args);
     }
-    //@Bean(name = "commandLineRunner") //@Profile("dev")
+    @Bean(name = "commandLineRunner") @Profile("dev")
     public CommandLineRunner start(TypeLoader typeLoader, FoldersIniter foldersIniter, AdminLoader adminLoader, StatutLoader statutLoader,
                                    DeviseLoader deviseLoader, PaysLoader paysLoader, PclLoader pclLoader,
                                    BrancheLoader brancheLoader, CouvertureLoader couvertureLoader,
                                    CedCesAffLoader cedCesAffLoader, ExerciceLoader exerciceLoader, CessionnaireRepository cesRepo
-                                  ,PrvLoader prvLoader, RoleLoader roleLoader,MenuLoader menuLoader, AssLoader assLoader)
+                                  ,PrvLoader prvLoader, RoleLoader roleLoader,MenuLoader menuLoader, AssLoader assLoader, NatureLoader natureLoader)
     {
         return args->{
             typeLoader.load();
@@ -49,6 +50,7 @@ public class SynchronReApplication {
             pclLoader.load();
             exerciceLoader.load();
             cedCesAffLoader.load();
+            natureLoader.load();
         };
      }
 }
