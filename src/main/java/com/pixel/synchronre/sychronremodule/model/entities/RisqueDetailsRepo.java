15aeea41f9696964ff1a44c978b73d75bef43062
lenicoulibaly <lenicoulibaly@gmail.com>
2024-05-19 00:40:39 +0000

    Ajout des sous couvertures à risques couverts

--- src/main/java/com/pixel/synchronre/sychronremodule/model/entities/RisqueDetailsRepo.java
+++ src/main/java/com/pixel/synchronre/sychronremodule/model/entities/RisqueDetailsRepo.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 public interface RisqueDetailsRepo extends JpaRepository<RisqueCouvertDetails, Long>
 {
-    @Query("select r from RisqueCouvertDetails r where r.risqueCouvert.risqueId = ?1 and r.risqueCouvert.couverture.couId = ?2")
+    @Query("select r from RisqueCouvertDetails r where r.risqueCouvert.risqueId = ?1 and r.couverture.couId = ?2")
     RisqueCouvertDetails findByRisqueIdAndSousCouId(Long risqueId, Long couId);
 
-    @Query("select (count(r.risqueDetailsId) > 0) from RisqueCouvertDetails r where r.risqueCouvert.risqueId = ?1 and r.risqueCouvert.couverture.couId = ?2")
+    @Query("select (count(r.risqueDetailsId) > 0) from RisqueCouvertDetails r where r.risqueCouvert.risqueId = ?1 and r.couverture.couId = ?2")
     boolean risqueHasSousCouverture(Long risqueId, Long couId);
 
 
@@ -21,7 +21,7 @@ public interface RisqueDetailsRepo extends JpaRepository<RisqueCouvertDetails, L
     List<Long> getCouIdsToRemove(Long risqueId, List<Long> couIds);
 
     @Query("""
-    select c.couId from Couverture c where c.couId not in ?2 and 
+    select c.couId from Couverture c where c.couId in ?2 and 
     not exists(select rd from RisqueCouvertDetails rd where rd.risqueCouvert.risqueId = ?1 and rd.couverture.couId = c.couId)
     """)
     List<Long> getCouIdsToAdd(Long risqueId, List<Long> couIds);
