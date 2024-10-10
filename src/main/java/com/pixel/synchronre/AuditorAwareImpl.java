4158ca4af3485fb4f24fd108d4eebfe73953b77b
lenicoulibaly <lenicoulibaly@gmail.com>
2024-10-10 14:36:35 +0000

    Ajout de envers pour l'historisation

---
+++ src/main/java/com/pixel/synchronre/AuditorAwareImpl.java
@@ -0,0 +1,20 @@
+package com.pixel.synchronre;
+
+import com.pixel.synchronre.authmodule.controller.services.spec.IJwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor @Component
+public class AuditorAwareImpl implements AuditorAware<String>
+{
+    private final IJwtService jwtService;
+    @Override
+    public Optional<String> getCurrentAuditor()
+    {
+        String extractedAuditor = jwtService != null ? jwtService.extractUsername() : "UNKNOWN";
+        return Optional.of(extractedAuditor==null || extractedAuditor.equals("")? "UNKNOWN": extractedAuditor );
+    }
+}
--- src/main/java/com/pixel/synchronre/SynchronReApplication.java
+++ src/main/java/com/pixel/synchronre/SynchronReApplication.java
@@ -1,20 +1,20 @@
 package com.pixel.synchronre;
 
-import com.pixel.synchronre.init.*;
-import com.pixel.synchronre.sychronremodule.model.dao.CessionnaireRepository;
-import com.pixel.synchronre.sychronremodule.model.entities.Organisation;
-import org.springframework.boot.CommandLineRunner;
+import com.pixel.synchronre.authmodule.controller.services.spec.IJwtService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.AuditorAware;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 
 @SpringBootApplication
-public class SynchronReApplication {
-
+public class SynchronReApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(SynchronReApplication.class, args);
+    }
     @Bean
     public DecimalFormat decimalFormat()
     {
@@ -25,36 +25,9 @@ public class SynchronReApplication {
         return decimalFormat;
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(SynchronReApplication.class, args);
-    }
-    //@Bean(name = "commandLineRunner") //@Profile("dev")
-    public CommandLineRunner start(TypeLoader typeLoader, FoldersIniter foldersIniter, AdminLoader adminLoader, StatutLoader statutLoader,
-                                   DeviseLoader deviseLoader, PaysLoader paysLoader, PclLoader pclLoader,
-                                   BrancheLoader brancheLoader, CouvertureLoader couvertureLoader,
-                                   CedCesAffLoader cedCesAffLoader, ExerciceLoader exerciceLoader
-                                  ,NatureLoader natureLoader, PrvLoader prvLoader, RoleLoader roleLoader, MenuLoader menuLoader, AssLoader assLoader,OrganisationLoader organisationLoader,
-                                  OrganisationPaysLoader organisationPaysLoader)
+    @Bean
+    public AuditorAware<String> auditorProvider(IJwtService jwtService)
     {
-        return args->{
-            typeLoader.load();
-            statutLoader.load();
-            menuLoader.load();
-            foldersIniter.load();
-            prvLoader.load();
-            roleLoader.load();
-            adminLoader.load();
-            assLoader.load();
-            deviseLoader.load();
-            brancheLoader.load();
-            couvertureLoader.load();
-            organisationLoader.load();
-            paysLoader.load();
-            organisationPaysLoader.load();
-            pclLoader.load();
-            exerciceLoader.load();
-            cedCesAffLoader.load();
-            natureLoader.load();
-        };
-     }
+        return new AuditorAwareImpl(jwtService);
+    }
 }
