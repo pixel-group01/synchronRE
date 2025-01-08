ef2ee42de4969d6bad8653b4d949c2b8a3b21ef0
lenicoulibaly <lenicoulibaly@gmail.com>
2025-01-08 10:48:41 +0000

    Ajout de la configuration de auditorAwareRef

--- src/main/java/com/pixel/synchronre/SynchronReApplication.java
+++ src/main/java/com/pixel/synchronre/SynchronReApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class SynchronReApplication
 {
     public static void main(String[] args) {
